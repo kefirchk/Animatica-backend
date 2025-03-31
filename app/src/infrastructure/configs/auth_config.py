eb47e490cf4cae2338df5cfcff6e9a58dbffb4fc
kefirchk <prostolex2004@mail.ru>
2025-03-31 03:09:57 +0300

    Added default values for auth config

--- app/src/infrastructure/configs/auth_config.py
+++ app/src/infrastructure/configs/auth_config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 
 
 class AuthConfig(BaseSettings):
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_DAYS: int
+    SECRET_KEY: str = Field("your-secret-key")
+    ALGORITHM: str = Field("HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(30)
+    REFRESH_TOKEN_EXPIRE_DAYS: int = Field(30)
     TOKEN_ISSUER: str = Field("Animatica")
