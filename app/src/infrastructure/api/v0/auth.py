37822060d0d3aa5a704e3a1eeecf70f7a5c62126
kefirchk <prostolex2004@mail.ru>
2025-04-11 21:15:23 +0300

    Updated password length condition

--- app/src/infrastructure/api/v0/auth.py
+++ app/src/infrastructure/api/v0/auth.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Depends, Query
+from fastapi import Body, Depends
 from pydantic import SecretStr
 from src.domain.usecases.auth import UserLoginUseCase, UserSignUpUseCase
 from src.infrastructure.api import APIRouter
@@ -11,8 +11,8 @@ router = APIRouter()
 @router.post("/login")
 async def login(
     use_case: Annotated[UserLoginUseCase, Depends(UserLoginUseCase)],
-    username: str = Query(..., min_length=3),
-    password: SecretStr = Query(..., min_length=4),
+    username: str = Body(..., min_length=3),
+    password: SecretStr = Body(..., min_length=8),
 ):
     return await use_case.execute(use_case.Request(username=username, password=password.get_secret_value()))
 
@@ -20,7 +20,7 @@ async def login(
 @router.post("/signup")
 async def signup(
     use_case: Annotated[UserSignUpUseCase, Depends(UserSignUpUseCase)],
-    username: str = Query(..., min_length=3),
-    password: SecretStr = Query(..., min_length=4),
+    username: str = Body(..., min_length=3),
+    password: SecretStr = Body(..., min_length=8),
 ):
     return await use_case.execute(use_case.Request(username=username, password=password.get_secret_value()))
