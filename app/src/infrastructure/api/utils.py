b403205be7b9bb7329f2f70baa6dcbb29776a81c
kefirchk <prostolex2004@mail.ru>
2025-05-07 22:31:05 +0300

    Removed add_api_websocket method from APIRouter

--- app/src/infrastructure/api/utils.py
+++ app/src/infrastructure/api/utils.py
@@ -11,7 +11,3 @@ class APIRouter(FastAPIRouter):
     def add_api_route(self, path: str, *args: Any, include_in_schema: bool = True, **kwargs: Any) -> None:
         super().add_api_route(_get_alternative_path(path), *args, include_in_schema=False, **kwargs)
         super().add_api_route(path, *args, include_in_schema=include_in_schema, **kwargs)
-
-    def add_api_websocket_route(self, path: str, *args: Any, **kwargs: Any) -> None:
-        super().add_api_websocket_route(_get_alternative_path(path), *args, **kwargs)
-        super().add_api_websocket_route(path, *args, **kwargs)
