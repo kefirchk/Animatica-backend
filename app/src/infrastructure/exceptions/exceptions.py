a661c4163197c9708e6ceeca4ac1ea0210366572
kefirchk <prostolex2004@mail.ru>
2025-05-07 23:02:31 +0300

    Removed NotFoundException class

--- app/src/infrastructure/exceptions/exceptions.py
+++ app/src/infrastructure/exceptions/exceptions.py
@@ -18,19 +18,6 @@ class InvalidTokenException(AuthException):
     """Raised when the provided token is invalid or cannot be recognized."""
 
 
-class NotFoundException(Exception):
-    """Base exception for all not found errors"""
-
-    entity_name: str = "Entity"
-
-    def __init__(self, entity_id=None):
-        self.entity_id = entity_id
-        message = f"{self.entity_name} not found"
-        if entity_id is not None:
-            message += f" with id {entity_id}"
-        super().__init__(message)
-
-
 class StripeException(Exception):
     """Base exception for Stripe product related errors."""
 
