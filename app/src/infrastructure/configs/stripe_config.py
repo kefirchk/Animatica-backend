63aab3657f6f93d62664f7e89baf24ca2ecd6c31
kefirchk <prostolex2004@mail.ru>
2025-05-09 16:08:48 +0300

    Updated env names for stripe.env

--- app/src/infrastructure/configs/stripe_config.py
+++ app/src/infrastructure/configs/stripe_config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class StripeConfig(BaseSettings):
-    PUBLIC_KEY: str = Field(..., alias="PUBLIC_KEY")
-    SECRET_KEY: str = Field(..., alias="SECRET_KEY")
+    PUBLIC_KEY: str = Field(..., alias="STRIPE_PUBLIC_KEY")
+    SECRET_KEY: str = Field(..., alias="STRIPE_SECRET_KEY")
 
     model_config = SettingsConfigDict(env_file="../env/stripe.env")
 
